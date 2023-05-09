4241bd06bf63c942e531c6ddefab0a146beaf6c9
Gedeonir <irafasha.jedy12@gmail.com>
2023-05-09 16:48:27 +0200

    Appointnments Endpoints

---
+++ models/doctors.js
@@ -0,0 +1,66 @@
+"use strict";
+import mongoose, { Schema } from "mongoose";
+const Doctor = mongoose.model(
+  "doctors",
+  new Schema({
+    title:{
+      type:String,
+    },
+    firstName: {
+      type: String,
+      required:true
+    },
+    lastName: {
+      type: String,
+      required:true
+    },
+    gender: {
+      type: String,
+      required:true
+    },
+    email: {
+      type: String,
+      required:true,
+      unique: true
+    },
+    telNumber: {
+      type: String,
+      required:true,
+    },
+    departmentId:{
+      type: mongoose.Schema.Types.ObjectId,
+      ref:"departments",
+      required:true
+    },
+    departmentName:{
+      type: String,
+    },
+    role:{
+      type:String
+    },
+    password: {
+      type: String,
+      required:true
+    },
+    passwordResetToken: {
+      type: String,
+      default: "",
+    },
+    profilePicture: {
+      type: String,
+      defaultValue:"",
+    },
+    accountActivationToken:{
+      type: String,
+      default: "",
+    },
+    active:{
+      type:Boolean,
+      default:false
+    },
+    createdAt:{
+      type:String
+    }
+  })
+);
+export {Doctor};
---
+++ src/Appointments/Appointment.route.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const {createAppointment,getAllAppoinments,getOneAppoinment}=require('./Appointment.controllers');
+
+const router = express.Router();
+
+// router.get("/",getAllAssignments);
+router.post("/send_appointment/:doctorId",createAppointment)
+router.get("/",getAllAppoinments);
+router.get("/:appointmentId",getOneAppoinment);
+module.exports=router;
