ae4c3f32db8a59eea3668261a28bb453ccff57ae
Gedeonir <irafasha.jedy12@gmail.com>
2023-05-15 12:21:44 +0200

    Deployment

--- models/departments.js
+++ models/departments.js
@@ -1,5 +1,6 @@
 "use strict";
-import mongoose, { Schema } from "mongoose";
+const mongoose=require("mongoose");
+const { Schema }=require("mongoose");
 const Department = mongoose.model(
     "departments",
     new Schema({
